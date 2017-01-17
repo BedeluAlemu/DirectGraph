acaf1db8e56645a39e17d9a8d4e019d0ca62a573
BedelAlemu <aaleemuu@gmail.com>
2017-01-17 14:27:01 -0800

    test data inserted

--- DirectGraph/DirectGraph/Program.cs
+++ DirectGraph/DirectGraph/Program.cs
@@ -10,6 +10,20 @@ namespace DirectGraph
     {
         static void Main(string[] args)
         {
+            DirectedGraph DG = new DirectedGraph();
+
+            DG.InsertVertex("xyz");
+            DG.InsertVertex("pqr");
+            DG.InsertVertex("lmn");
+            DG.InsertVertex("abc");
+
+            DG.InserEdge("xyz", "pqr");
+            DG.InserEdge("xyz", "abc");
+            DG.InserEdge("pqr", "abc");
+            DG.InserEdge("abc", "lmn");
+            DG.Display();
+            Console.ReadLine();
+
         }
     }
 }
